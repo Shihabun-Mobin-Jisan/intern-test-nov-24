7f5be2cb4c55cec0c55a1174e1fab4754859b773
Sakib-Rahman-Ratin <ratin989@gmail.com>
2024-11-07 19:21:30 +0600

    try to complete the task

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageController;
+
+
 
 Route::get('/', function () {
     return view('upload');
+  
 });
+
+Route::post('/upload-image', [ImageUploadController::class, 'upload'])->name('image.upload');
