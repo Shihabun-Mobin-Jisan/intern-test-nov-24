9691fa317a5768b6a0cbb6e0173da47ea95724ca
miraz66 <mirajulislam0509@gmail.com>
2024-11-07 16:30:44 +0600

    Complate Test

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreImageRequest;
 use App\Http\Resources\ImageResource;
 use App\Models\Image;
-use Illuminate\Contracts\Cache\Store;
-use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
@@ -20,11 +18,7 @@ class ImageController extends Controller
     public function store(StoreImageRequest $request)
     {
         $file = $request->file('image');
-
         $filename = time() . '.' . $file->getClientOriginalExtension();
-
-        // $imagePath = $request->file('image')->store('images', 'public');
-
         $imagePath = $file->storeAs('images', $filename, 'public');
 
         Image::create([
